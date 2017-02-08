003bdffff7882a5e70888f76679eb7930601091b
Julie C <Julie C>
2017-02-07 19:09:46 -0500

    almost done with vuforia part, need to have transitions be seamless

---
+++ BootcampVuforia2/Assets/Scripts/CubeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	enum TransformMode {
+		Translate,
+		Rotate,
+		Scale
+	};
+
+	public GameObject targetToFollow;
+	// Use this for initialization
+	private TransformMode mode = TransformMode.Translate;
+
+	Vector3 targetLastPos;
+	void Start () {
+		targetLastPos = targetToFollow.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("t")) {
+			mode = TransformMode.Translate;
+		} else if (Input.GetKeyUp ("r")) {
+			mode = TransformMode.Rotate;
+		} else if (Input.GetKeyUp ("s")) {
+			mode = TransformMode.Scale;
+		}
+
+		if (mode == TransformMode.Translate) {
+			//Debug.Log ("cube x " + transform.position.x);
+			//Debug.Log ("target from cube x " + targetToFollow.transform.position.x);
+			Vector3 translationAmount = targetToFollow.transform.position - targetLastPos;
+			//transform.position = targetToFollow.transform.position;
+			transform.position += translationAmount;
+			targetLastPos = targetToFollow.transform.position;
+		} else if (mode == TransformMode.Rotate) {
+			transform.rotation = targetToFollow.transform.rotation;
+		} else {
+
+			//left it gets small, right it gets big
+			float scaleFactor = targetToFollow.transform.position.x;
+			scaleFactor = scaleFactor > 0 ? scaleFactor : 0;
+			//Debug.Log ("scalefactor " + scaleFactor);
+			Vector3 myNewScale = new Vector3 (scaleFactor, scaleFactor, scaleFactor);
+			//Vector3 lol = Vector3 (2, 2, 2);
+			transform.localScale = myNewScale;
+		}
+
+	}
+}
---
+++ BootcampVuforia2/Assets/Scripts/TargetController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("target x " + transform.position.x);
+		Debug.Log ("target y " + transform.position.y);
+		Debug.Log ("target z " + transform.position.z);
+
+	}
+}
